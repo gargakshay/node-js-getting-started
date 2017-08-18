8a56213fb9e6ea6c48ecbd10fb060b09e0d23dde
akshay garg <gargakshay2010@gmail.com>
2017-08-18 14:52:25 +0530

    Added UI

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 var express = require('express');
+var path = require('path');
+var index = require('./routes/index');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'angular/dist/')));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  // response.render('pages/index');
-  response.send("hello1222323S");
-});
+
+app.use('/', index);
+
+// app.get('/', function(request, response) {
+//   // response.render('pages/index');
+//   response.send("hello1222323S");
+// });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
---
+++ routes/index.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var multer  = require('multer');
+var storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './uploads');
+  }
+});
+
+var upload = multer({ storage : storage }).any();
+
+var csv = require('csvtojson');
+
+var router = express.Router();
+
+router.get('/', function(req, res, next){
+    res.sendFile('index.html');
+});
+
+router.post('/upload', function(req, res, next){
+
+    upload(req, res, function(err) {
+
+        if(err){
+            res.sendStatus(400);
+            return;
+        }
+        
+        var arrFiles = req.files;
+        var arrData = [];
+
+        for(let i = 0; i < arrFiles.length; i++){
+            //Converting CSV to JSON
+            csv()
+            .fromFile(arrFiles[i].path)
+            .on("end_parsed", function(jsonObj){
+                arrData.push({data: jsonObj, filename: arrFiles[i].originalname});
+                
+                // When file is last then send Response
+                if(i == arrFiles.length - 1){
+                    res.send(arrData);
+                }
+            })
+            .on('error',(err)=>{
+                res.sendStatus(400);
+            });        
+        }
+    });
+});
+
+module.exports = router;
